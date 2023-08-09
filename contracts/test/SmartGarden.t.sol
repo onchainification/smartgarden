2d55dbce3bf8f4218ec1ef747e4334f5137f5b71
Petrovska <petrovska@badger.com>
2023-08-09 21:55:39 +0200

    feat: hook architecture agents in test

--- contracts/test/SmartGarden.t.sol
+++ contracts/test/SmartGarden.t.sol
@@ -1,9 +1,21 @@
 //SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
+pragma solidity ^0.8.20;
 
 /* Testing utilities */
 import {Test} from "forge-std/Test.sol";
 
+import {Safe} from "safe-contracts/Safe.sol";
+import {SafeProtocolRegistry} from "safe-protocol/SafeProtocolRegistry.sol";
 import {SmartGardenManager} from "../src/SmartGardenManager.sol";
 
-contract SmartGardenTest is Test {}
+contract SmartGardenTest is Test {
+  // ecosystem agents
+  address owner = address(14);
+
+  // gnosis safe
+  Safe safe = new Safe();
+
+  // safe-protocol sc
+  SafeProtocolRegistry registry = new SafeProtocolRegistry(owner);
+  SmartGardenManager manager = new SmartGardenManager(owner, address(registry));
+}
