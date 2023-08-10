f466dcb2551cca89767b6bd84c9a47c47cc1f9dd
Petrovska <petrovska@badger.com>
2023-08-10 17:07:11 +0200

    feat: rename interace from module -> plugin

--- contracts/src/SmartGardenManager.sol
+++ contracts/src/SmartGardenManager.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import {SafeProtocolManager} from "safe-protocol/SafeProtocolManager.sol";
 import {ISafeProtocolPlugin} from "safe-protocol/interfaces/Integrations.sol";
 
-import {IModule} from "./interfaces/IModule.sol";
+import {IPlugin} from "./interfaces/IPlugin.sol";
 
 contract SmartGardenManager is SafeProtocolManager {
   constructor(
@@ -12,11 +12,10 @@ contract SmartGardenManager is SafeProtocolManager {
     address _registry
   ) SafeProtocolManager(_initialOwner, _registry) {}
 
-  // TODO: add another method which accepts config in `enablePlugin`
   function enablePluginWithConfig(
     address plugin,
     bool allowRootAccess,
-    IModule.DummyConfig calldata config
+    IPlugin.DummyConfig calldata config
   ) external noZeroOrSentinelPlugin(plugin) onlyPermittedPlugin(plugin) {
     PluginAccessInfo storage senderSentinelPlugin = enabledPlugins[msg.sender][
       SENTINEL_MODULES
@@ -41,8 +40,7 @@ contract SmartGardenManager is SafeProtocolManager {
     senderPlugin.rootAddressGranted = allowRootAccess;
     senderSentinelPlugin.nextPluginPointer = plugin;
 
-    // Tweak config here?
-    IModule(plugin).setSafeConfig(msg.sender, config);
+    IPlugin(plugin).setSafeConfig(msg.sender, config);
 
     emit PluginEnabled(msg.sender, plugin, allowRootAccess);
   }
--- contracts/src/interfaces/IPlugin.sol
+++ contracts/src/interfaces/IPlugin.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-interface IModule {
+interface IPlugin {
   struct DummyConfig {
     address vault;
     uint64 cadenceSec;
