b83cc3c57eb3a1350cc5b85b198a56ac603c5ec0
Gosuto Inzasheru <gosuto@inzasheru.email>
2023-08-15 14:07:14 +0200

    fix: endpoints get archived too quick

--- src/components/GraphQuery.tsx
+++ src/components/GraphQuery.tsx
@@ -1,7 +1,7 @@
 export async function getBalance(_address: string) {
   // define url and query
   var url =
-    "https://api.studio.thegraph.com/query/50162/smartgarden/v0.0.2d";
+    "https://api.studio.thegraph.com/query/50162/smartgarden/v0.0.2e";
   var query = `{
     gaugePosition(id: "${_address}") {
       balance
