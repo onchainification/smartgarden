ff1e8ca5d3778a8d1056f38b6b79fbe38f4840e4
Petrovska <petrovska@badger.com>
2023-08-16 23:52:21 +0200

    feat: add gnosis safe sdk and update enabler component

--- src/components/ManagerEnabler.tsx
+++ src/components/ManagerEnabler.tsx
@@ -1,25 +1,33 @@
-import { useAccount, useContractWrite } from "wagmi";
+import { encodeFunctionData } from "viem";
+
+import useGnosisBatch from "../queries/useGnosisBatch";
 
 import { safeABI, smartGardenManagerAddress } from "../generated";
 
-import { ProcessingMessage } from "./HashProcessor";
+// import { ProcessingMessage } from "./HashProcessor";
 
 export function ManagerEnabler({ addr }: { addr?: `0x${string}` }) {
-  const { data, isLoading, write } = useContractWrite({
-    address: addr,
-    abi: safeABI,
-    functionName: "enableModule",
-  });
+  const { mutate: gnosisBatch } = useGnosisBatch();
+
+  const handleClick = () => {
+    const call: { [key: string]: string[] } = {};
+    call[addr] = [
+      encodeFunctionData({
+        abi: safeABI,
+        functionName: "enableModule",
+        args: [smartGardenManagerAddress[10]],
+      }),
+    ];
+
+    gnosisBatch({ calls: call });
+  };
 
   return (
     <div>
-      <button
-        style={{ marginLeft: 20 }}
-        onClick={() => write({ args: [smartGardenManagerAddress[10]] })}
-      >
+      <button style={{ marginLeft: 20 }} onClick={handleClick}>
         Enable Smart Garden Manager in your safe!
       </button>
-      {isLoading && <ProcessingMessage hash={data?.hash} />}
+      {/*isLoading && <ProcessingMessage hash={data?.hash} />*/}
     </div>
   );
 }
