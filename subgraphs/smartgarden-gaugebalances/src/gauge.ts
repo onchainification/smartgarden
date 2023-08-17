79f52f374d4afb106d39f438ba5de83458dbea53
Gosuto Inzasheru <gosuto@inzasheru.email>
2023-08-17 10:18:26 +0200

    fix: deposits can occur on behalf of other user

--- subgraphs/smartgarden-gaugebalances/src/gauge.ts
+++ subgraphs/smartgarden-gaugebalances/src/gauge.ts
@@ -12,24 +12,22 @@ export function getUser(address: Bytes): User {
 }
 
 export function handleDeposit(event: Deposit): void {
-  if (event.params.amount > BigInt.fromI32(0)) {
-    let user = getUser(event.params.from);
-    let gaugePosition = GaugePosition.load(
+  let user = getUser(event.params.to);
+  let gaugePosition = GaugePosition.load(
+    event.address.toHexString() + "-" + user.id.toHexString(),
+  );
+  if (!gaugePosition) {
+    gaugePosition = new GaugePosition(
       event.address.toHexString() + "-" + user.id.toHexString(),
     );
-    if (!gaugePosition) {
-      gaugePosition = new GaugePosition(
-        event.address.toHexString() + "-" + user.id.toHexString(),
-      );
-      gaugePosition.gauge = event.address;
-      gaugePosition.user = user.id;
-      gaugePosition.balance = event.params.amount;
-      gaugePosition.save();
-      return;
-    }
-    gaugePosition.balance = gaugePosition.balance.plus(event.params.amount);
+    gaugePosition.gauge = event.address;
+    gaugePosition.user = user.id;
+    gaugePosition.balance = event.params.amount;
     gaugePosition.save();
+    return;
   }
+  gaugePosition.balance = gaugePosition.balance.plus(event.params.amount);
+  gaugePosition.save();
 }
 
 export function handleWithdraw(event: Withdraw): void {
