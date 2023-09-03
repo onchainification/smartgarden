a92f261480f1f2686f4da4af4c30cca710c9a34d
Gosuto Inzasheru <gosuto@inzasheru.email>
2023-09-03 15:28:55 +0200

    feat: add link prices and harvest cost

--- subgraphs/smartgarden-optimism-upkeeps/src/mapping.ts
+++ subgraphs/smartgarden-optimism-upkeeps/src/mapping.ts
@@ -1,13 +1,25 @@
-import { BigInt, ethereum, log } from "@graphprotocol/graph-ts";
-import { Transfer } from "../generated/KeeperRegistry1_3/ERC20";
-import { UpkeepPerformed as UpkeepPerformedEvent } from "../generated/KeeperRegistry1_3/KeeperRegistry1_3";
+import {
+  Address,
+  BigInt,
+  ethereum,
+  // log,
+} from "@graphprotocol/graph-ts";
+// import { Transfer } from "../generated/KeeperRegistry1_3/ERC20";
+import {
+  KeeperRegistry1_3,
+  UpkeepPerformed as UpkeepPerformedEvent,
+} from "../generated/KeeperRegistry1_3/KeeperRegistry1_3";
 import { UpkeepPerformed, Harvest } from "../generated/schema";
 import { tuplePrefixBytes } from "./utils";
+import { Oracle } from "../generated/KeeperRegistry1_3/Oracle";
 
 // TODO: retrieve dynamically from smart garden registry?
 const HARVESTER_PLUGIN_ID = BigInt.fromString(
   "15275435444678090841310445038221187480372252783087795255404194648563901208280",
 );
+const LINK = KeeperRegistry1_3.bind(
+  Address.fromString("0x75c0530885F385721fddA23C539AF3701d6183D4"),
+).LINK();
 
 export function handleUpkeepPerformed(event: UpkeepPerformedEvent): void {
   // harvester plugin logic
@@ -44,16 +56,20 @@ export function handleUpkeepPerformed(event: UpkeepPerformedEvent): void {
 
     // new harvest object for each multisig
     for (let i = 0; i < addresses.length; i++) {
-      let msig = addresses[i];
-      let harvest = new Harvest(event.transaction.hash.concat(msig));
+      let account = addresses[i];
+      let harvest = new Harvest(event.transaction.hash.concat(account));
 
-      harvest.msig = msig;
+      harvest.account = account;
+      harvest.n_accounts = BigInt.fromI32(addresses.length);
 
       // from UpkeepPerformed event:
       harvest.hash = event.transaction.hash;
       harvest.block = event.block.number;
       harvest.timestamp = event.block.timestamp;
       harvest.link_used = event.params.payment;
+      harvest.link_used_per_account = event.params.payment.div(
+        BigInt.fromI32(addresses.length),
+      );
 
       // TODO: for each Transfer event {
       // if (event.params.to == harvest.msig) {
@@ -65,9 +81,20 @@ export function handleUpkeepPerformed(event: UpkeepPerformedEvent): void {
       // }
       // }
 
-      // TODO: calcs. needs price sdk
-      // harvest.cost = link_used * list_price / 10^18 / n_harvests;
-      // harvest.revenue = amount * token_price / 10^18;
+      let linkusd = Oracle.bind(
+        Address.fromString("0xCc232dcFAAE6354cE191Bd574108c1aD03f86450"),
+      );
+      harvest.link_price = linkusd
+        .latestAnswer()
+        .divDecimal(BigInt.fromI32(10).pow(8).toBigDecimal()); // linkusd decimals
+      harvest.cost = harvest.link_used
+        .divDecimal(BigInt.fromI32(10).pow(18).toBigDecimal()) // link decimals
+        .times(harvest.link_price);
+      harvest.cost_per_account = harvest.link_used_per_account
+        .divDecimal(BigInt.fromI32(10).pow(18).toBigDecimal()) // link decimals
+        .times(harvest.link_price);
+
+      // harvest.revenue = harvest.amount * token_price / 10^18;
       // harvest.profit = harvest.revenue - harvest.cost;
 
       harvest.save();
